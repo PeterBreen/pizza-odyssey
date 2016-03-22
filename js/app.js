493b6d994fd55c77d88d7dbe6d5bf3f677283812
Peter Breen <pbreen@gmail.com>
2016-03-22 11:53:08 -0700

    scope working properly for all array calculations

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 //declare empty global variables here
 var pizzasMade; // for total pizzas per day across all stores
-var pizzasDelivered; // ???
-var deliveriesMadeHour = [];
-var pizzasMadeHour = [];
-var driversPerHour = [];
 
 // big ugly gross global arrays
 var hoursOperation = ['8:00am','9:00am','10:00am','11:00am','12 noon','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm','8:00pm','9:00pm','10:00pm','11:00pm','12 midnight','1:00am'];
@@ -11,6 +7,7 @@ var minPizzasHourFloor = [0,0,0,0,0,0,2,2,2,15,15,15,12,12,12,5,5,5];
 var maxPizzasHourCeiling = [4,4,4,7,7,7,15,15,15,35,35,35,31,31,31,20,20,20];
 var minDeliveriesHour = [0,0,0,0,0,0,1,1,1,3,3,3,5,5,5,6,6,6];
 var maxDeliveriesHour = [4,4,4,4,4,4,4,4,4,8,8,8,12,12,12,11,11,11];
+var storeNameArray = [ballard, firstHill, international, ravenna, slu, georgetown];
 //make sure these all read the same number - should be 18
 console.log ('minpizzashour length: ' + minPizzasHourFloor.length);
 console.log ('maxpizzashour length: ' + maxPizzasHourCeiling.length);
@@ -47,11 +44,11 @@ function getRandomIntInclusive(min, max) {
 function populateStoreObject(storeName) {
   calculatePizzasSold(storeName);
   calculatePizzasDelivered(storeName);
-  calculateDeliveryDrivers(storeName);
 }
 
 //calculate pizzas sold per hour - use random function and really big and ugly global arrays above
 function calculatePizzasSold(storeName) {
+  var pizzasMadeHour = [];
   for (i = 0; i < hoursOperation.length; i++) {
     pizzasCalc = getRandomIntInclusive(minPizzasHourFloor[i], maxPizzasHourCeiling[i]);
     pizzasMadeHour.push(pizzasCalc);
@@ -60,17 +57,14 @@ function calculatePizzasSold(storeName) {
 }
 
 //calculate deliveries made per hour - similar to calculating pizzas sold per hour
+//ALSO calculates delivery drivers per hour (keeping it in same scope BECAUSE ARGH)
 function calculatePizzasDelivered(storeName) {
+  var deliveriesMadeHour = [];
+  var driversPerHour = [];
   for (i = 0; i < hoursOperation.length; i++) {
     deliveriesCalc = getRandomIntInclusive(minDeliveriesHour[i], maxDeliveriesHour[i]);
     deliveriesMadeHour.push(deliveriesCalc);
-  }
-  storeName.pizzasDelivered = deliveriesMadeHour;
-}
-
-function calculateDeliveryDrivers(storeName) {
-  for (i = 0; i < hoursOperation.length; i++) {
-    if (deliveriesMadeHour[i] === 0) {
+    if (deliveriesCalc === 0) {
       driversPerHour.push('driver not reccomended');
     } else {
       //return "drivers reccomended: x" where x is deliveries/3 rounded UP to this.driversHired
@@ -78,5 +72,6 @@ function calculateDeliveryDrivers(storeName) {
       driversPerHour.push('drivers reccomended: ' + numDrivers);
     }
     storeName.driversHired = driversPerHour;
+    storeName.pizzasDelivered = deliveriesMadeHour;
   }
 }
